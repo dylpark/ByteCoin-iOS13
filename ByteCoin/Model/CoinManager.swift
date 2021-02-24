f11edb7d4444eea55bc2226c27fb9f76e8668b4f
Dylan Park <dylanpark.design@gmail.com>
2021-02-24 10:27:42 +1000

    Update CoinManager.swift

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -2,8 +2,8 @@
 //  CoinManager.swift
 //  ByteCoin
 //
-//  Created by Angela Yu on 11/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Dylan Park on 24/02/2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
 //
 
 import Foundation
@@ -14,6 +14,9 @@ struct CoinManager {
     let apiKey = "YOUR_API_KEY_HERE"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
+    
+    func getCoinPrice(for currency: String) {
+        
+    }
     
 }
